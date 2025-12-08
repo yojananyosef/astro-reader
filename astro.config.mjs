b2f52bcedc6a3d7cd910f98d51ad38575c3c2eeb
Johan Gutierrez <johan.gutierrez@outlook.cl>
2025-12-08 19:57:36 -0300

    feat: Add Netlify adapter with hybrid output, remove compressor integration, and update gitignore.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: netlify(),
   integrations: [preact()],
 
   vite: {
